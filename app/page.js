79288aa72c4ff1fec5fea726b84f0a71000550c1
Adhham Safwan <msmadhhamg@gmail.com>
2023-12-05 04:08:25 +0000

    Update client ID and client secret in API calls

--- app/page.js
+++ app/page.js
@@ -5,8 +5,9 @@ export default async function Home() {
   const router = useSearchParams();
   const unsplashCode = router.get('code');
   console.log(unsplashCode)
-  const clientSecret = process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_SECRET || 'pTpi0sTLDCMlvxrJ8am2H_Z6x7Dg28Ie2FLjaPK5qQM';
-  const authorized = await fetch(`https://unsplash.com/oauth/token?client_id=eq_nBtpFvjy3KgmsPIcrmGXEsQ7-g7F1FWlJ2OOz01I&client_secret=${clientSecret}&redirect_uri=https://adhham-pictoria.netlify.app/&code=${unsplashCode}&grant_type=authorization_code`, {method: 'POST'})
+  const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET || process.env.CLIENT_SECRET;
+  const clientId = process.env.NEXT_PUBLIC_CLIENT_ACCESS_KEY || process.env.CLIENT_ACCESS_KEY;
+  const authorized = await fetch(`https://unsplash.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=https://adhham-pictoria.netlify.app/&code=${unsplashCode}&grant_type=authorization_code`, {method: 'POST'})
   console.log(authorized)
   return (
     <main>
--- next.config.js
+++ next.config.js
@@ -5,10 +5,11 @@ const nextConfig = {
     },
     
     async redirects() {
+        const clientId = process.env.NEXT_PUBLIC_CLIENT_ACCESS_KEY || process.env.CLIENT_ACCESS_KEY;
         return [
             {
                 source: "/",
-                destination: "https://unsplash.com/oauth/authorize?client_id=eq_nBtpFvjy3KgmsPIcrmGXEsQ7-g7F1FWlJ2OOz01I&redirect_uri=https://adhham-pictoria.netlify.app/&response_type=code&scope=public+read_user+write_likes",
+                destination: `https://unsplash.com/oauth/authorize?client_id=${clientId}&redirect_uri=https://adhham-pictoria.netlify.app/&response_type=code&scope=public+read_user+write_likes`,
                 permanent: true,
             },
         ];
