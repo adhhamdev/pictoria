cd6dd175cf26523ef15c9e404cc18488d2b9dcf0
Adhham Safwan <msmadhhamg@gmail.com>
2023-12-05 03:59:06 +0000

    Remove unnecessary code and add authorization for Unsplash API

--- app/page.js
+++ app/page.js
@@ -1,6 +1,13 @@
 import Gallery from '@/components/Gallery'
 import Shimmer from '@/components/Shimmer'
+import { useSearchParams } from 'next/navigation'
 export default async function Home() {
+  const router = useSearchParams();
+  const unsplashCode = router.get('code');
+  console.log(unsplashCode)
+  const clientSecret = process.env.NEXT_PUBLIC_UNSPLASH_CLIENT_SECRET || 'pTpi0sTLDCMlvxrJ8am2H_Z6x7Dg28Ie2FLjaPK5qQM';
+  const authorized = await fetch(`https://unsplash.com/oauth/token?client_id=eq_nBtpFvjy3KgmsPIcrmGXEsQ7-g7F1FWlJ2OOz01I&client_secret=${clientSecret}&redirect_uri=https://adhham-pictoria.netlify.app/&code=${unsplashCode}&grant_type=authorization_code`, {method: 'POST'})
+  console.log(authorized)
   return (
     <main>
       <div className="banner">
--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [{hostname: "images.unsplash.com", pathname: "**", protocol: "https"}, {hostname: "api.unsplash.com", pathname: "**", protocol: "https"}]
+        remotePatterns: [{hostname: "images.unsplash.com", pathname: "**", protocol: "https"}, {hostname: "api.unsplash.com", pathname: "**", protocol: "https"}],
+    },
+    
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "https://unsplash.com/oauth/authorize?client_id=eq_nBtpFvjy3KgmsPIcrmGXEsQ7-g7F1FWlJ2OOz01I&redirect_uri=https://adhham-pictoria.netlify.app/&response_type=code&scope=public+read_user+write_likes",
+                permanent: true,
+            },
+        ];
     },
 }
 
