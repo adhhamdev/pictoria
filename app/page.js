e4889fdc56157d80f637c7ddf0a42bc7665ce3d4
Adhham Safwan <msmadhhamg@gmail.com>
2023-12-05 04:39:40 +0000

    Update environment variable usage

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ export default async function Home() {
   const router = useSearchParams();
   const unsplashCode = router.get('code');
   console.log(unsplashCode)
-  const clientSecret = process.env.NEXT_PUBLIC_CLIENT_SECRET || process.env.CLIENT_SECRET;
-  const clientId = process.env.NEXT_PUBLIC_CLIENT_ACCESS_KEY || process.env.CLIENT_ACCESS_KEY;
+  const clientSecret = process.env.CLIENT_SECRET;
+  const clientId = process.env.CLIENT_ACCESS_KEY;
   const authorized = await fetch(`https://unsplash.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=https://adhham-pictoria.netlify.app/&code=${unsplashCode}&grant_type=authorization_code`, {method: 'POST'})
   console.log(authorized)
   return (
