1675294850a781bfb2332ed151bad05779386e8e
adhhamdev <msmadhhamg@gmail.com>
2024-04-10 03:35:49 +0530

    Refactor authentication middleware and update access token retrieval

--- middleware.js
+++ middleware.js
@@ -1,14 +1,29 @@
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
-export const middleware = (req) => {
-  let params = req.nextUrl.search
+const clientSecret = process.env.CLIENT_SECRET;
+const clientId = process.env.NEXT_PUBLIC_CLIENT_ID;
+const redirectUri = process.env.REDIRECT_URI;
+
+export const middleware = async (req) => {
+  let params = req.nextUrl.search;
   const url = new URLSearchParams(params);
-  const code = url.get('code');
-  const response = NextResponse.next();
-  response.cookies.set('code', code)
-  return response
-}
+  const code = url.get("code");
+  req.cookies.set("code", code);
+  const res = await fetch(
+    `https://unsplash.com/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&redirect_uri=${redirectUri}&code=${code}&grant_type=authorization_code`,
+    { method: "POST" }
+  );
+  // response.cookies.set("token", accessToken);
+  // console.log("authentication", code, accessToken)
+  // console.log(req.nextUrl);
+  // if (code) {
+  //   const authData = await res.json();
+  //   const accessToken = authData.access_token;
+    
+  //   const response = NextResponse.next();
+  // }
+};
 
 export const config = {
-  matcher: '/'
-}
+  matcher: "/",
+};
